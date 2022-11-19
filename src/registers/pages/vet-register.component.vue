909da3f70dc2bc42ee096ed074d128837f3fc3fa
Lynn Jeerferzon <81722082+LynnJ-Meza@users.noreply.github.com>
2022-11-19 12:27:09 -0500

    feat(vetregister): create vet register.

---
+++ src/registers/pages/vet-register.component.vue
@@ -0,0 +1,49 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "vet-register.component"
+
+  data(){
+    return{
+      vetRegisters:[],
+      vetRegistersDialog:false,
+      deletevetRegisterDialog:false,
+      vetRegister:{},
+      vetRegisterService:null
+    };
+  },
+  created() {
+  },
+  methods:{
+    getStorableTutorial(displayablevetRegister) {
+      return {
+        id: displayablevetRegister.id,
+        name: displayablevetRegister.name,
+        ruc: displayablevetRegister.ruc,
+        address: displayablevetRegister.address,
+        phone: displayablevetRegister.phone,
+      };
+    },
+      openNew(){
+      this.vetRegister={};
+      this.vetRegisterDialog=true;
+      },
+
+    hideDialog(){
+      this.vetRegisterDialog=false;
+    },
+    findIndexByid(id){
+      console.log(`current id:${id}`);
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
