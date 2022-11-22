e87f730309cc15f8d3e7b16d43a1d086c267f89e
bruno1923 <u201914974@upc.edu.pe>
2022-11-22 14:00:40 -0500

    delete

--- src/profile/services/user.service.js
+++
@@ -1,9 +0,0 @@
-import http from "../../core/services/http-common";
-
-class UsersService{
-    getByEmailAndPassword(email, password){
-        return http.get(`/users?email=${email}&password=${ password }`);
-
-    }
-}
-export default new UsersService
