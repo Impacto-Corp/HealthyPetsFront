f72d87be35e5af178afc2bd0d293f653930d63f4
bruno1923 <u201914974@upc.edu.pe>
2022-11-22 12:01:01 -0500

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
+import PetProfileComponent from "@/profiles/pages/pet-profile.component.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'PetProfile',
+      component: PetProfileComponent
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/petprofile',
+      name: 'Petprofile',
+      component: PetProfileComponent,
     }
   ]
 })
